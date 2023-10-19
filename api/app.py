bddc01b4f2ce2a1ec31bf94db0a2c9017bd13b9e
PDA44719 <zceepde@ucl.ac.uk>
2023-10-19 13:40:50 +0100

    Included basic error checking to improve robustness.

--- api/app.py
+++ api/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import math
 
 app = Flask(__name__)
+
+def valid_number(number):
+    # Only int values greater than 0 are allowed
+    try:
+        number_int = int(number) # Convert from str to int
+    except:
+        return False
+    if number_int <= 0:
+        return False
+    else:
+        return True
 
 def prime_number_check(number):
     counter = 2
@@ -13,13 +24,19 @@ def prime_number_check(number):
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html") 
 
 #this is the function for submitting the number
 @app.route("/submit", methods=["POST"])
 def submit():
     input_number = request.form.get("number")
+    if not valid_number(input_number):
+        return redirect("/error")
     is_prime = prime_number_check(int(input_number))
     image_src = "happy.jpeg" if is_prime else "sad.jpeg"
     return render_template("prime_check.html", number=input_number, is_prime=is_prime, image_src=image_src) 
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
